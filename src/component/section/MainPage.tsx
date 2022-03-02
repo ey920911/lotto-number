188158e503c6f96900d9b5f921c3d2c820165e99
eykim <ey920911@naver.com>
2022-03-02 19:19:20 +0900

    feat: Add up to number 25 dream words

---
+++ src/component/section/MainPage.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { Button, Select, MenuItem } from '@material-ui/core';
+import {
+  LottoFooter,
+  ThemeSection,
+  Dream,
+  RandomNumber,
+  Birth,
+} from 'component';
+
+function MainPage() {
+  const [lottoLine, setLottoLine] = React.useState(5);
+
+  const handleChange = (event: any) => {
+    setLottoLine(event.target.value);
+  };
+  return (
+    <div className="main-section">
+      <Select value={lottoLine} label="number_line" onChange={handleChange}>
+        <MenuItem value={5}>5</MenuItem>
+        <MenuItem value={10}>10</MenuItem>
+        <MenuItem value={20}>20</MenuItem>
+        <MenuItem value={30}>30</MenuItem>
+      </Select>
+      <ThemeSection />
+      <Switch>
+        <Route path="/random">
+          <RandomNumber line={lottoLine} />
+        </Route>
+        <Route path="/dream">
+          <Dream line={lottoLine} />
+        </Route>
+        <Route path="/birth">
+          <Birth line={lottoLine} />
+        </Route>
+      </Switch>
+      <Redirect from="*" to="/" />
+    </div>
+  );
+}
+
+export default MainPage;
