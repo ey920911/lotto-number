e0ae2ec3eb64a4689d273d58cf7cd121bd9328f4
eykim <ey920911@naver.com>
2022-07-26 18:40:50 +0900

    feat: lotto icon 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Card, CardContent } from '@material-ui/core';
 import { LottoHeader, MainPage, LottoFooter } from 'component';
 
 function App() {
--- src/component/section/LottoHeader.tsx
+++ src/component/section/LottoHeader.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import lottoIcon from 'icon/lotto_icon.png';
 
 function LottoHeader() {
-  return <div className="main-section">Lotto 자동 생성기</div>;
+  return (
+    <div className="main-section">
+      <img className="lotto-icon-16" src={lottoIcon} alt="lotto logo" />
+      Lotto 자동 생성기
+    </div>
+  );
 }
 export default LottoHeader;
