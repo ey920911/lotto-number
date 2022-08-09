454882d05ab0d5c979ebad109c25fb785618c6ee
eykim <ey920911@naver.com>
2022-08-09 16:42:42 +0900

    chore: Add git repo url

--- src/component/dialog/DescriptContent.jsx
+++ src/component/dialog/DescriptContent.jsx
@@ -7,28 +7,26 @@ import descriptData from './description.json';
 function DescriptContent(props) {
   const [value, setValue] = useState('random');
 
-  // const handleChange = (event: any, newValue: any) => {
-  //   setValue(newValue);
-  // };
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  const data = descriptData.description;
+  const { description, genMode } = descriptData;
+  const descArr = Object.keys(description);
 
   return (
     <Box sx={{ width: '30rem', height: '10rem', typography: 'body1' }}>
       <TabContext value={value}>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <TabList onChange={handleChange} aria-label="descript-tabList">
-            {Object.keys(data).map((theme) => (
-              <Tab label={theme} value={theme} />
+            {descArr.map((theme) => (
+              <Tab label={genMode[theme]} value={theme} />
             ))}
           </TabList>
         </Box>
-        <TabPanel value="birth"> {data['birth']} </TabPanel>
-        <TabPanel value="random"> {data['random']} </TabPanel>
-        <TabPanel value="dream"> {data['dream']} </TabPanel>
+        {descArr.map((theme) => (
+          <TabPanel value={theme}> {description[theme]} </TabPanel>
+        ))}
       </TabContext>
     </Box>
   );
