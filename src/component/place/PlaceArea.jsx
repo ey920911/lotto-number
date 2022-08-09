a021a6fc7e170df5e9d35687fa8f54c26110f85e
eykim <ey920911@naver.com>
2022-08-09 23:12:42 +0900

    feat: Infinite scroll function on hot place page

--- src/component/place/PlaceArea.jsx
+++ src/component/place/PlaceArea.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { makeStyles } from '@material-ui/core';
 import PlaceItem from './PlaceDataContainer.jsx';
 import { PLACE_DATA } from 'data/PositionData';
@@ -10,12 +10,39 @@ const useStyles = makeStyles({
 function PlaceArea({ name = 'all' }) {
   const classes = useStyles();
   const placeList = Object.entries(PLACE_DATA[name]);
+  const loader = useRef(null);
+  const [visiblePage, setVisiblePage] = useState(1);
+
+  // init visiblePage value
+  useEffect(() => {
+    setVisiblePage(1);
+  }, [name]);
+
+  useEffect(() => {
+    const option = {
+      root: null,
+      rootMargin: '20px',
+      threshold: 0,
+    };
+    const observer = new IntersectionObserver(handleObserver, option);
+    console.log('loader.current', loader.current);
+    if (loader.current) observer.observe(loader.current);
+  }, [handleObserver]);
+
+  const handleObserver = useCallback((entries) => {
+    const target = entries[0];
+    if (target.isIntersecting) {
+      setVisiblePage((prev) => prev + 1);
+    }
+  }, []);
 
   return (
     <div className={classes.placeContainer}>
-      {placeList.map(([name, data]) => (
-        <PlaceItem name={name} data={data} />
-      ))}
+      {placeList.map(([name, data], index) => {
+        if (index > visiblePage) return <></>;
+        return <PlaceItem name={name} data={data} />;
+      })}
+      <div ref={loader} />
     </div>
   );
 }
